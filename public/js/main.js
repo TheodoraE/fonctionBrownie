82592f5d8056e19f0d9ccf1cde8b0b5edd375c25
TheodoraE <tidoraa@gmail.com>
2020-11-16 14:45:24 +0100

    init

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+
+// Créer une fonction qui permettra de calculer les proportions par rapport au nombre de personnes
+
+// A prendre en compte
+    // valeur pour nombres de personnes doit être entier (prompt)
+    // valeur pour Oeuf doit être entier (souci au niveau pâtisserie => pour aller plus loin, régler ce problème et adapter les autres ingrédients si oeuf à été ajouté ou supprimé : REALISME)
+
